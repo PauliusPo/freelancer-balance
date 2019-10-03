84c1277bd86d23f700685369be413e741d99e4d1
PauliusPo <54659793+PauliusPo@users.noreply.github.com>
2019-10-03 20:49:58 +0300

    tabble data

--- js/action.js
+++ js/action.js
@@ -1,1 +1,2 @@
 renderTableContent('.table-content .months-name', months );
+renderTableContent('.table-content .', months );
--- js/function.js
+++ js/function.js
@@ -2,5 +2,6 @@ renderMonths(target, data) {
     let MonthHTML = '';
     for ( let i=0; i<data.length ; i++) {
         MonthHTML += `<div class="table-content">
-                    <div class="months-name">
+                    <div class="months-name"${data[i].months}>`
+};
 }
