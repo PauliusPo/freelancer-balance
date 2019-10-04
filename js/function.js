6564d63edd9bc509000a4d834844dbc5cf58853e
PauliusPo <54659793+PauliusPo@users.noreply.github.com>
2019-10-04 12:20:13 +0300

    There da hell is 0

--- js/function.js
+++ js/function.js
@@ -1,17 +1,37 @@
 function renderTable(target, data) {
-
     let accountHTML = '';
-    for ( let i=0; i<data.length ; i++) {
+    let balance = "";
+    for ( let i=0; i<data.length ; i++){
+        if  (typeof(data[i].income) == undefined) {
+            data[i].income = 0;
+            (typeof(data[i].expense) == undefined) 
+                data[i].expense = 0;
+        }
+        
         accountHTML += `<div class="table-row">
                         <div class="cell">${i+1}</div>
                         <div class="cell">${data[i].month}</div>
                         <div class="cell">${data[i].income}Eur</div>
                         <div class="cell">${data[i].expense}Eur</div>
-                        <div class="cell">150.00 Eur</div>
+                        <div class="cell">${data[i].income - data[i].expense}Eur</div>
                         </div>`;
                         
                                      
-    
+
     } 
     return document.querySelector(target).innerHTML = accountHTML;
 }
+
+
+
+// jeigu income !== skaicius, tada duok man 0;
+
+   // }
+        // if (!data[i].income && data[i].expense) {
+        //     balance = data[i].expense;
+        // }
+        // if (data[i].income && !data[i].expense) {
+        //     balance = data[i].income;
+        // }
+       
+
